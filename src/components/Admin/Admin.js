0dd22d39115c08fde71f94afecc311b3a8be55cb
NamNguyen205 <namxautrai205@gmail.com>
2025-09-17 21:49:03 +0700

    day7

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,18 +1,24 @@
-import SideBar from "./SideBar"
+import SideBar from "./SideBar";
+import "./SideBar.scss";
+import "react-pro-sidebar/dist/css/styles.css";
 
-const Admin = (props) =>{
-    return (
-        <>
-            <div className = "admin-container">
-                <div className="admin-sidebar">
-                    <SideBar />
-                </div>
-                <div className="admin-content">
-                    content
-                </div>
-            </div>
-        </>
-    )
-}
+const Admin = (props) => {
+  return (
+    <>
+      <div className="admin-container">
+        <div className="admin-sidebar">
+          <SideBar
+            
+            collapsed={false}
+            rtl={false}
+            toggled={false}
+            handleToggleSidebar={() => {}}
+          />
+        </div>
+        <div className="admin-content">content</div>
+      </div>
+    </>
+  );
+};
 
 export default Admin;
